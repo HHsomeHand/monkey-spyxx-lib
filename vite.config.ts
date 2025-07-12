d7b11db6a6aa6aaa62210186744c3b8c40106567
宫仅 <hhsomehand@gmail.com>
2025-07-12 15:10:55 +0800

    init: vite.config.ts 添加路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import monkey, { cdn } from 'vite-plugin-monkey';
+import path from 'path'
+
+function resolve(pathName: string) {
+  return path.resolve(__dirname, pathName)
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve('src'),
+      '@components': resolve( 'src/components'),
+      '@utils': resolve('src/utils'),
+      // 可以根据项目需求添加更多别名
+    },
+  },
   plugins: [
     react(),
     monkey({
