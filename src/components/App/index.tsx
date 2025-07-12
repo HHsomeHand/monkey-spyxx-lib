83cc72d62738191047deda89de4ab6bfc97db4eb
宫仅 <hhsomehand@gmail.com>
2025-07-12 15:11:12 +0800

    init: 调整目录结构

---
+++ src/components/App/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {memo} from "react";
+
+export const App = memo((props) => {
+    return (
+        <div>
+            Hello world!
+        </div>
+    );
+});
+
+export default App;
