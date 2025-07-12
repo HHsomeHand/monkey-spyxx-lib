2c1d3ff7d4a11ad192eb99828368280837d1c771
宫仅 <hhsomehand@gmail.com>
2025-07-12 17:32:09 +0800

    init: 引入 anti design

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {memo} from "react";
+import {Button} from "antd";
 
 export const App = memo((props) => {
     return (
         <div className="absolute size-20 bg-amber-400 top-0 left-0">
-            Hello world!
+            <Button type="primary">Button</Button>
         </div>
     );
 });
