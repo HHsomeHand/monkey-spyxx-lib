1e4c1b5d5398763233b9eb0c21e3a09d5e52df96
宫仅 <hhsomehand@gmail.com>
2025-07-14 13:15:11 +0800

    refactor: App 重命名为 CornApp

---
+++ src/components/CornApp/index.tsx
@@ -0,0 +1,28 @@
+import clsx from 'clsx';
+import React, {useEffect, useRef} from 'react';
+import {memo} from "react";
+import {makeDraggable} from "@/utils/makeDraggable.ts";
+import {AppProps} from "antd";
+
+interface CornAppProps {
+    className?: string,
+    children?: React.ReactNode,
+}
+
+export const CornApp = memo((
+    props: CornAppProps
+) => {
+
+    return (
+        <div
+            className={clsx(
+                props.className,
+                "corn-app"
+            )}
+        >
+            {props.children}
+        </div>
+    );
+});
+
+export default CornApp;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from "@/components/App";
 import {cornDebugModeHint} from "@/assets/ascii-art/debug-mode-hint.ts";
 import {ISpyXX} from "@/types";
 import "@/assets/css/global.css";
 import UserSelectDialog from "@/components/Dialogs/UserSelectDialog";
+import {CornApp} from "@/components/CornApp";
 
 function renderDialog(dialog: React.ReactNode) {
     return ReactDOM.createRoot(
@@ -26,7 +26,9 @@ const spyXX: ISpyXX = {
             }
 
             renderDialog(
-                <UserSelectDialog onResult={onResult}/>
+                <CornApp>
+                    <UserSelectDialog onResult={onResult}/>
+                </CornApp>
             );
         });
     },
