bcf89d4e72a001e87cfe2c2b003c33e671d717a2
宫仅 <hhsomehand@gmail.com>
2025-07-17 17:01:00 +0800

    bugfix: getSelector 修复了 "明明兄弟元素类名和目标元素类名不一致, 还 :nth-child" 的 bug

--- src/utils/getSelector.ts
+++ src/utils/getSelector.ts
@@ -7,8 +7,11 @@ export function getSelector(element: Element) {
 
         if (current.id) {
             path.unshift(`#${current.id}`);
+
             break;
         }
+
+        let isElHasClassName = false;
 
         if (current.className) {
             const classes = String(current.className).split(' ').filter(c => c).join('.');
@@ -16,10 +19,43 @@ export function getSelector(element: Element) {
             if (classes) {
                 selector += `.${classes}`;
             }
+
+            isElHasClassName = true;
+        }
+
+        function hasSameClasses(element1: Element, element2: Element): boolean {
+            // 将第一个元素的类名存入 Set
+            const classes1 = new Set(element1.classList);
+
+            // 将第二个元素的类名存入 Set
+            const classes2 = new Set(element2.classList);
+
+            // 检查两个 Set 的大小是否相同
+            if (classes1.size !== classes2.size) {
+                return false;
+            }
+
+            // 检查每个类名是否同时存在于两个 Set 中
+            for (const className of classes1) {
+                if (!classes2.has(className)) {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         const siblings = Array.from(current.parentElement!.children).filter(
-            sibling => sibling.tagName === current!.tagName
+            sibling => {
+                let isSameTag = sibling.tagName === current!.tagName;
+
+                let isSameClassName = true;
+
+                if (isElHasClassName) {
+                    isSameClassName = hasSameClasses(sibling, current!);
+                }
+                return isSameTag && isSameClassName;
+            }
         );
 
         if (siblings.length > 1) {
