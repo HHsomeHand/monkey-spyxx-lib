75b242d9b44b3102742b0871b37b137ecf6b4c32
宫仅 <hhsomehand@gmail.com>
2025-07-18 22:10:20 +0800

    ui: UserSelectorDisplayer 设置了滑块的颜色

--- src/components/dialog/UserSelectDialog/c-cpns/CornSelectorDisplayer/index.tsx
+++ src/components/dialog/UserSelectDialog/c-cpns/CornSelectorDisplayer/index.tsx
@@ -61,6 +61,21 @@ export const CornSelectorDisplayer = memo((
 
     const _Slider = (
         <input
+            className="w-full h-2 bg-neutral-200 rounded-lg appearance-none cursor-pointer accent-neutral-500
+                [&::-webkit-slider-thumb]:appearance-none
+                [&::-webkit-slider-thumb]:h-4
+                [&::-webkit-slider-thumb]:w-4
+                [&::-webkit-slider-thumb]:rounded-full
+                [&::-webkit-slider-thumb]:bg-neutral-500
+                [&::-webkit-slider-thumb]:shadow-md
+                [&::-webkit-slider-thumb]:hover:bg-neutral-600
+
+                [&::-moz-range-thumb]:appearance-none
+                [&::-moz-range-thumb]:h-4
+                [&::-moz-range-thumb]:w-4
+                [&::-moz-range-thumb]:rounded-full
+                [&::-moz-range-thumb]:bg-neutral-500
+            "
             type="range"
             min="0"
             max={propCurrSelectorArr.length - 1}
