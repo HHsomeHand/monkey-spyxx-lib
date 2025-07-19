64360d8e85e8ebe23fd6ad88f3c824eb9ccde99a
宫仅 <hhsomehand@gmail.com>
2025-07-19 21:27:57 +0800

    chore: vite.config.ts 更新版本号

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ import tailwindcss from "@tailwindcss/vite";
 let userscript: MonkeyUserScript = {
     namespace: "npm/vite-plugin-monkey",
     author: "QQ2402398917",
-    version: "1.0.4",
+    version: "1.0.5",
     license: "MIT",
 };
 
